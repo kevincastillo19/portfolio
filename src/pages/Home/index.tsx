15fbe96caa035da5f1f31375542a6bddbb0bb98d
kevin <kevincastillo19@hotmail.com>
2023-03-01 19:23:25 -0500

    feat: finish cards in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import styles from './homeStyles.module.scss'
 import { Card } from '../../components/Card/index'
 import laptopIcon from '../../assets/img/icons/laptop-icon.svg'
 import tabletIcon from '../../assets/img/icons/tablet-icon.svg'
+import ruleIcon from '../../assets/img/icons/rule-icon.svg'
 export const Home = () => {
     const CAPTION_TEXT =
         "I'm a System engineer with more than three years of experience in Software development, currently I'm working as a Fullstack developer. In addition to this, I have experience in the graphic design area, such as Design UX/UI and others."
@@ -32,7 +33,15 @@ export const Home = () => {
                       title="UX/UI Design"
                       caption="I have experience with UX/UI Design, Let’s start to make a new unicorn."
                       buttonText='Know more'
-                      buttonAction={()=> console.log("know more fullstack")}
+                      buttonAction={()=> console.log("know more design")}
+                  />
+                  <Card
+                      icon={ruleIcon}
+                      color="gray"
+                      title="Branding"
+                      caption="I have experience with UX/UI Design, Let’s start to make a new unicorn."
+                      buttonText='Know more'
+                      buttonAction={()=> console.log("know more branding")}
                   />
                 </div>
             </section>
