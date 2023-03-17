6b6474f2dca34ea3e0fef0a674464bc0285e4224
kevin <kevincastillo19@hotmail.com>
2023-03-16 22:04:12 -0500

    feat:add feed photo

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../Buttons/Button";
 import styles from "./_feed.module.scss";
+import feedPhoto from "../../assets/img/feed-photo.png";
 type props = {
   title: string;
   caption: string;
@@ -8,6 +9,7 @@ type props = {
 export const Feed = ({ title, caption, label }: props) => {
   return (
     <div className={styles.container}>
+      <img src={feedPhoto} alt="profile-photo"/>
       <div className={styles.data}>
         <span className={styles.titleLabel}>{label}</span>
         <h1 className={styles.title}>{title}</h1>
