1b395e1eeeecbb9a3318319d8e9560d090a70a1e
Kevin Castillo <kevincastillo19@hotmail.com>
2023-06-11 21:47:37 -0500

    reset vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    rollupOptions:{
-      output:{
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  }
+  plugins: [react()]
 })
